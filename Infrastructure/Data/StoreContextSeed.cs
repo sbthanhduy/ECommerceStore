c9f9c1485aa002b06e9adcf3790568441b40d89f
Thanh Duy <sb.thanhduy@gmail.com>
2025-05-22 21:12:35 +0700

    Product embedding seed for Redis

--- Infrastructure/Data/StoreContextSeed.cs
+++ Infrastructure/Data/StoreContextSeed.cs
@@ -2,12 +2,17 @@
 using System.Text.Json;
 using Core.Entities;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.VectorData;
+using Microsoft.SemanticKernel.Embeddings;
+#pragma warning disable SKEXP0001
 
 namespace Infrastructure.Data;
 
 public class StoreContextSeed
 {
-    public static async Task SeedAsync(StoreContext context, UserManager<AppUser> userManager)
+    public static async Task SeedAsync(StoreContext context, UserManager<AppUser> userManager,
+        ITextEmbeddingGenerationService textEmbeddingGenerationService,
+        IVectorStoreRecordCollection<string, ProductVector> vectorStoreRecordCollection)
     {
         if (!userManager.Users.Any(x => x.UserName == "admin@test.com"))
         {
@@ -35,8 +40,44 @@ public class StoreContextSeed
             context.Products.AddRange(products);
 
             await context.SaveChangesAsync();
+            
+            await vectorStoreRecordCollection.CreateCollectionIfNotExistsAsync();
+
+            var persitedProduct = context.Products.AsAsyncEnumerable();
+            
+            var productVectors = new List<ProductVector>();
+            await foreach (Product p in persitedProduct)
+            {
+                await Task.Delay(6000);
+                var embedding = await textEmbeddingGenerationService.GenerateEmbeddingAsync(p.Description);
+                productVectors.Add(new ProductVector()
+                {
+                    Id = p.Id.ToString(),
+                    ProductId = p.Id,
+                    Name = p.Name,
+                    Price = p.Price,
+                    Description = p.Description,
+                    Type = p.Type,
+                    Brand = p.Brand,
+                    DescriptionEmbedding = embedding
+                });
+            }
+            if (productVectors.Any())
+            {
+                await foreach (var key in vectorStoreRecordCollection.UpsertBatchAsync(productVectors))
+                {
+                    // Process each key as they come in
+                    Console.WriteLine($"Upserted item with key: {key}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No valid product vectors to upsert.");
+            }
+           
         }
 
+        
         if (!context.DeliveryMethods.Any())
         {
             var dmData = await File
@@ -50,5 +91,7 @@ public class StoreContextSeed
 
             await context.SaveChangesAsync();
         }
+
+        
     }
 }
